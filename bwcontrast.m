ca631ba3801049f1319ca1b8627df560fd957d9e
Eaton <Kenneth.Eaton@cchmc.org>
2017-03-21 16:41:57 -0400

    First commit.

---
+++ bwcontrast.m
@@ -0,0 +1,10 @@
+function bwData = bwcontrast(colorData)
+%bwcontrast   Generate black or white contrasts for RGB color triples.
+
+% Author: Ken Eaton
+% Version: MATLAB R2016b
+% Last modified: 3/21/17
+% Copyright 2017 by Kenneth P. Eaton
+%--------------------------------------------------------------------------
+
+end
